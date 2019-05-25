e4ad35e719e526fee80b5dbec38194cfa577fb52
eclipse_user <eclipse_user@atguigu.com>
2019-05-25 17:38:41 +0800

    test hot_fix...

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -4,5 +4,8 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("workspace2 ...");
 		System.out.println("workspace ...");
+		
+		
+		System.out.println("test hot_fix...");
 	}
 }
